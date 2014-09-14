73f3b09927127362306da7ada3c7ced6cf1348ca
samy arous <samy.arous@gmail.com>
2014-09-14 12:11:14 +0200

    the story begins...

--- server.js
+++ server.js
@@ -20,12 +20,36 @@ app.get('/config/environment', function() {
 
 // /user(s) API
 var R = require('./server/resource');
+app.post('/api/cards', R.Cards.list);
+app.post('/api/cards/children', R.Cards.children);
+app.post('/api/cards/count', R.Cards.count);
+app.post('/api/cards/upvote', R.Cards.upvote);
+app.post('/api/cards/star', R.Cards.star);
+app.post('/api/cards/unstar', R.Cards.unstar);
+app.post('/api/card/addlabel', R.Cards.addlabel);
+app.post('/api/cards/removelabel', R.Cards.removelabel);
+app.post('/api/cards/clearlabels', R.Cards.clearlabels);
+app.post('/api/cards/act', R.Cards.act);
+app.post('/api/cards/create', R.Cards.create);
+app.post('/api/cards/update', R.Cards.update);
+app.post('/api/cards/remove', R.Cards.remove);
+
 app.get('/api/cards', R.Cards.list);
-app.get('/api/test/cards', R.Cards.test);
+app.get('/api/cards/children', R.Cards.children);
+app.get('/api/cards/count', R.Cards.count);
+app.get('/api/cards/upvote', R.Cards.upvote);
+app.get('/api/cards/star', R.Cards.star);
+app.get('/api/cards/unstar', R.Cards.unstar);
+app.get('/api/cards/addlabel', R.Cards.addlabel);
+app.get('/api/cards/removelabel', R.Cards.removelabel);
+app.get('/api/cards/clearlabels', R.Cards.clearlabels);
+app.get('/api/cards/act', R.Cards.act);
+app.get('/api/cards/create', R.Cards.create);
+app.get('/api/cards/update', R.Cards.update);
+app.get('/api/cards/remove', R.Cards.remove);
 
-
-//app.get(/\/user\/(.+?)\/checks/, R.Checks.list);
-//app.get(/\/user\/(.+?)\/check_count/, R.Users.check_count);
+//app.post(/\/user\/(.+?)\/checks/, R.Checks.list);
+//app.post(/\/user\/(.+?)\/check_count/, R.Users.check_count);
 //app.post('/payments/:amount', R.Payments.create_transaction);
 
 mach.serve(app, 9200);
