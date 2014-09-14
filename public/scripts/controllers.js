c2a5ea371bf0fd7a48654abe676014d8c248beba
jp.pequito <jp.pequito@cupenya.com>
2014-09-14 14:25:18 +0200

    hardcoded grid

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -11,6 +11,7 @@ angular.module('myApp.controllers', [])
 		function($scope, $http, $location) {
 			$scope.foo = "hello";
 
+		var container = document.querySelector('#container');
 	      var msnry = new Masonry( "#container", {
 	        // options
 	        columnWidth: 140,
@@ -75,7 +76,7 @@ angular.module('myApp.controllers', [])
 					controller: ModalInstanceCtrl,
 //					size: size,
 					backdrop: false,
-//					windowTemplateUrl: 'partials/Modal.html',
+					windowTemplateUrl: 'partials/Modal.html',
 					resolve: {
 						card: function () {
 							return card || {
